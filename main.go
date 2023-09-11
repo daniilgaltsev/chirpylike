381bae7685dfb1fba8896b6455606df220a265a9
Daniil Galtsev <galtsev.daniil@gmail.com>
2023-09-11 11:25:17 +0000

    add healthz and separate fileserver

--- main.go
+++ main.go
@@ -18,11 +18,24 @@ func middlewareCors(next http.Handler) http.Handler {
 	})
 }
 
+func healthHanlder(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
 func main() {
 	fmt.Println("Starting server")
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle(
+		"/app/",
+		http.StripPrefix(
+			"/app/",
+			http.FileServer(http.Dir(".")),
+		),
+	)
+	mux.HandleFunc("/healthz", healthHanlder)
 
 	corsMux := middlewareCors(mux)
 
